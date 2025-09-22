5c79462f7311ea590a36c914a069c136dbc246f1
SiravichJaruwatree <titan.jaru@gmail.com>
2025-09-22 15:11:12 +0700

    Update argv.sh

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+if [ $# -eq 0 ]; 
+then
+    echo "No arguments supplied"
+elif [ $# -le 3 ]; then
+    for arg in "$@"; do
+        echo $arg
+    done
+else
+    echo "Too many arguments"
+fi
